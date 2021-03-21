2c98febe0c8662e584786e01c6090900347c7aec
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2021-03-21 16:52:13 +0900

    test

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import RPi.GPIO as GPIO
 import time
 
 
-led_pin = 11
+led_green_pin = 20
+led_red_pin = 16
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led_pin, GPIO.OUT)
-    GPIO.output(led_pin, GPIO.LOW)
+    GPIO.setup(led_green_pin, GPIO.OUT)
+    GPIO.output(led_green_pin, GPIO.LOW)
+    GPIO.setup(led_red_pin, GPIO.OUT)
+    GPIO.output(led_red_pin, GPIO.LOW)
+
 
 def loop():
     while True:
-        GPIO.output(led_pin, GPIO.HIGH)
+        GPIO.output(led_green_pin, GPIO.HIGH)
+        GPIO.output(led_red_pin, GPIO.LOW)
         time.sleep(1)
-        GPIO.output(led_pin, GPIO.LOW)
+        GPIO.output(led_red_pin, GPIO.HIGH)
+        GPIO.output(led_green_pin, GPIO.LOW)
         time.sleep(1)
 
 if  __name__ == '__main__':
